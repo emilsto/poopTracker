f736427a6c491662d4b613ca2b39915777e78148
es <emil.kantaneva@tuni.fi>
2021-09-10 16:38:48 +0300

    minor UI changes, code formatting

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,30 +1,26 @@
+//make x from menuicon, apply animation change
 function xBar(x) {
     x.classList.toggle("change");
   } 
 
 function fadeOut(){
+  //variables
   let background = document.getElementById("secondaryContent");
-    background.classList.toggle("fadeOut");
+  let itemOptions1 = document.getElementById("testObject1");
+  let itemOptions2 = document.getElementById("testObject2");
+  let itemOptions3 = document.getElementById("testObject3");
   let menuBox = document.getElementById("testItem");
+
+  //fade background, apply fade effect
+  itemOptions1.classList.toggle("fadeIn1");
+  itemOptions2.classList.toggle("fadeIn2");
+  itemOptions3.classList.toggle("fadeIn3");
+  background.classList.toggle("fadeOut");
+
+  //make this as above
   if (menuBox.style.display != "block"){
     menuBox.style.display = "block";
   }
   else 
     menuBox.style.display = "none";
-  let itemOptions1 = document.getElementById("testObject1");
-  let itemOptions2 = document.getElementById("testObject2");
-  let itemOptions3 = document.getElementById("testObject3");
-    itemOptions1.classList.toggle("fadeIn1");
-    itemOptions2.classList.toggle("fadeIn2");
-    itemOptions3.classList.toggle("fadeIn3");
-
 }
-
-
-
-  function drinkRandomizer(){
-    let ingridientList = {vesi, maito, viini};
-
-    
-  }
-
